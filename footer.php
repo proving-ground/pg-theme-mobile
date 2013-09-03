42abc9f193f5677f98d022417fb2d900b37e19e9
Kevin Bachman <kbachman@provingground.net>
2013-09-03 16:16:56 -0400

    edited footer

--- footer.php
+++ footer.php
@@ -2,10 +2,14 @@
 		<div id="footerwrap">
 			<div id="footer">
 				<div id="footer-social-links">
-				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-facebook.png" />
-				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-google.png" />
-				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-twitter.png" />
-				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-linkedin.png" />
+				   <a href="https://www.facebook.com/proving" border="0">
+				                   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-facebook.png" /></a>
+				   <a href="https://plus.google.com/u/0/116625911901364822612/about" border="0">
+                   				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-google.png" /></a>
+				   <a href="https://twitter.com/ProvingGrnd" border="0">
+                   				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-twitter.png" /></a>
+				   <a href="http://www.linkedin.com/company/proving-ground-llc" border="0">
+                   				   <img src="<?php bloginfo( 'template_url' ); ?>/img/icon-linkedin.png" /></a>
 				</div>
 				<p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
 				<p><a href="privacy-policy/">Privacy Policy</a> | <a href="Contact/">Contact</a></p>
